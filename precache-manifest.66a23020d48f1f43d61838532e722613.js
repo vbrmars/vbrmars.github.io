e879d0fddb97daca1d716d6d69436cf3e9452d70
vbrmars <85068209+vbrmars@users.noreply.github.com>
2021-05-30 20:00:53 +0530

    Add files via upload

---
+++ precache-manifest.66a23020d48f1f43d61838532e722613.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7babdfb6c3cd0074fda850a139f14950",
+    "url": "/index.html"
+  },
+  {
+    "revision": "675400a5786652097137",
+    "url": "/static/css/main.69b3a264.chunk.css"
+  },
+  {
+    "revision": "fc51118b5e495af41217",
+    "url": "/static/js/2.9c54659e.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.9c54659e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "675400a5786652097137",
+    "url": "/static/js/main.ab440c11.chunk.js"
+  },
+  {
+    "revision": "c307e69827cf23a9bfac",
+    "url": "/static/js/runtime-main.b8b9f84b.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "a131039e0703f92f8f6ac57ca168cba9",
+    "url": "/static/media/front-img1.a131039e.jpg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "58a31115f5eb44daf89b9bbebc5c5ac1",
+    "url": "/static/media/logo.58a31115.png"
+  },
+  {
+    "revision": "40c5706e4740e83057ab98145174f72f",
+    "url": "/static/media/products.40c5706e.jpg"
+  },
+  {
+    "revision": "24107c283f779e8e8ada151233b8581f",
+    "url": "/static/media/wholesale.24107c28.jpg"
+  }
+]);
